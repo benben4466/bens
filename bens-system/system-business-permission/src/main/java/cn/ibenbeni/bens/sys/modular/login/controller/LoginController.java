3afa5ca71046f1846c6fa3b44d3160bb8f55190d
benben <ibenbeni@163.com>
2025-07-24 19:56:30 +0800

    feat: 新增登出系统接口

--- bens-system/system-business-permission/src/main/java/cn/ibenbeni/bens/sys/modular/login/controller/LoginController.java
+++ bens-system/system-business-permission/src/main/java/cn/ibenbeni/bens/sys/modular/login/controller/LoginController.java
@@ -1,8 +1,10 @@
 package cn.ibenbeni.bens.sys.modular.login.controller;
 
+import cn.hutool.core.util.StrUtil;
 import cn.ibenbeni.bens.auth.api.AuthServiceApi;
 import cn.ibenbeni.bens.auth.api.pojo.auth.AuthLoginReq;
 import cn.ibenbeni.bens.auth.api.pojo.auth.AuthLoginResp;
+import cn.ibenbeni.bens.auth.api.util.CommonLoginUserUtils;
 import cn.ibenbeni.bens.rule.pojo.response.ResponseData;
 import cn.ibenbeni.bens.rule.pojo.response.SuccessResponseData;
 import io.swagger.v3.oas.annotations.Operation;
@@ -33,6 +35,16 @@ public class LoginController {
     @PostMapping("/system/auth/login")
     public ResponseData<AuthLoginResp> login(@RequestBody @Valid AuthLoginReq loginReq) {
         return new SuccessResponseData<>(authServiceApi.login(loginReq));
+    }
+
+    @Operation(summary = "登出系统")
+    @PostMapping("/system/auth/logout")
+    public ResponseData<Boolean> logout() {
+        String userToken = CommonLoginUserUtils.getToken();
+        if (StrUtil.isNotBlank(userToken)) {
+            authServiceApi.logout();
+        }
+        return new SuccessResponseData<>(true);
     }
 
 }
