59fc99d5d90d929ce6b4da3d75421b91b0bba2c0
benben <ibenbeni@163.com>
2025-07-24 19:39:58 +0800

    bug: 修复MD5密码加密代码缺失

--- bens-auth/auth-sdk-customize/src/main/java/cn/ibenbeni/bens/auth/customize/password/MD5PasswordEncryptionStrategy.java
+++ bens-auth/auth-sdk-customize/src/main/java/cn/ibenbeni/bens/auth/customize/password/MD5PasswordEncryptionStrategy.java
@@ -1,5 +1,7 @@
 package cn.ibenbeni.bens.auth.customize.password;
 
+import cn.hutool.core.util.RandomUtil;
+import cn.hutool.crypto.SecureUtil;
 import cn.ibenbeni.bens.auth.api.password.PasswordEncryptionStrategy;
 import cn.ibenbeni.bens.auth.api.pojo.password.SaltedEncryptResult;
 
@@ -10,12 +12,18 @@ public class MD5PasswordEncryptionStrategy implements PasswordEncryptionStrategy
 
     @Override
     public SaltedEncryptResult encryptWithSalt(String originPassword) {
-        return null;
+        String salt = RandomUtil.randomString(8);
+        String encryptAfter = SecureUtil.md5(originPassword + salt);
+
+        SaltedEncryptResult saltedEncryptResult = new SaltedEncryptResult();
+        saltedEncryptResult.setPasswordSalt(salt);
+        saltedEncryptResult.setEncryptPassword(encryptAfter);
+        return saltedEncryptResult;
     }
 
     @Override
     public Boolean checkPasswordWithSalt(String encryptBefore, String passwordSalt, String encryptAfter) {
-        return null;
+        return SecureUtil.md5(encryptBefore + passwordSalt).equals(encryptAfter);
     }
 
 }
