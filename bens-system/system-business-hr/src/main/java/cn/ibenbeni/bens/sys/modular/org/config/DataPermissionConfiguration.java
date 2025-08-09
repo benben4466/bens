80900e85a8914ff2ed9dd8595b4c02b79d2bac8b
benben <ibenbeni@163.com>
2025-08-09 14:44:38 +0800

    docs: 暂时取消部门自定义数据权限提供者

--- bens-system/system-business-hr/src/main/java/cn/ibenbeni/bens/sys/modular/org/config/DataPermissionConfiguration.java
+++ bens-system/system-business-hr/src/main/java/cn/ibenbeni/bens/sys/modular/org/config/DataPermissionConfiguration.java
@@ -8,17 +8,17 @@ import org.springframework.context.annotation.Configuration;
 /**
  * 部门数据权限自动配置类
  */
-@Configuration(proxyBeanMethods = false)
-public class DataPermissionConfiguration {
-
-    /**
-     * 注入部门数据权限规则定制器
-     */
-    @Bean
-    public DeptDataPermissionRuleCustomizer sysDeptDataPermissionRuleCustomizer() {
-        return rule -> {
-            rule.addDeptColumn(HrOrganizationDO.class, "org_id");
-        };
-    }
-
-}
+//@Configuration(proxyBeanMethods = false)
+//public class DataPermissionConfiguration {
+//
+//    /**
+//     * 注入部门数据权限规则定制器
+//     */
+//    @Bean
+//    public DeptDataPermissionRuleCustomizer sysDeptDataPermissionRuleCustomizer() {
+//        return rule -> {
+//            rule.addDeptColumn(HrOrganizationDO.class, "org_id");
+//        };
+//    }
+//
+//}
