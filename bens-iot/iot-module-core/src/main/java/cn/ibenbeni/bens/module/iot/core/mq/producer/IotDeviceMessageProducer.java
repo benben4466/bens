7b6e6c1ea1b685fdc28380489e1fdd386e99864f
benben <ibenbeni@163.com>
2025-11-24 10:53:49 +0800

    [docs] 增加发送设备消息日志[测试使用]

--- bens-iot/iot-module-core/src/main/java/cn/ibenbeni/bens/module/iot/core/mq/producer/IotDeviceMessageProducer.java
+++ bens-iot/iot-module-core/src/main/java/cn/ibenbeni/bens/module/iot/core/mq/producer/IotDeviceMessageProducer.java
@@ -4,10 +4,12 @@ import cn.ibenbeni.bens.module.iot.core.messagebus.core.IotMessageBus;
 import cn.ibenbeni.bens.module.iot.core.mq.message.IotDeviceMessage;
 import cn.ibenbeni.bens.module.iot.core.util.IotDeviceMessageUtils;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * IoT-设备消息-生产者
  */
+@Slf4j
 @RequiredArgsConstructor
 public class IotDeviceMessageProducer {
 
@@ -19,6 +21,7 @@ public class IotDeviceMessageProducer {
      * @param message 设备消息
      */
     public void sendDeviceMessage(IotDeviceMessage message) {
+        log.info("[sendDeviceMessage][发送设备消息({})]", message);
         messageBus.post(IotDeviceMessage.MESSAGE_BUS_DEVICE_MESSAGE_TOPIC, message);
     }
 
@@ -29,6 +32,7 @@ public class IotDeviceMessageProducer {
      * @param message  设备消息
      */
     public void sendDeviceMessageToGateway(String serverId, IotDeviceMessage message) {
+        log.info("[sendDeviceMessageToGateway][发送设备消息({}/{})]", serverId, message);
         messageBus.post(IotDeviceMessageUtils.buildMessageBusGatewayDeviceMessageTopic(serverId), message);
     }
 
